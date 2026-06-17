af7765da03cf262cabd564b2472cbcfccaa8447c
Mian Zohaib <rafiqzohaib515@gmail.com>
2026-06-17 16:24:57 +0500

    Taweaks

--- index.js
+++ index.js
@@ -17,13 +17,7 @@ const allowedOrigins = [
 ].filter(Boolean);
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(null, false);
-        }
-    },
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
